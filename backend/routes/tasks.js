b9b1ecef2fcd98a6b05c1422fe8bdf20e11c0432
Ryan Huie <ryanhuie@Ryans-MacBook-Air.local>
2023-09-07 11:31:00 -0700

    Added delete and update tasks

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { createTask, getAllTasks, getCurrentUserTasks } from '../controllers/task.js';
+import {
+  createTask, getAllTasks, getCurrentUserTasks, updateTask, deleteTask,
+} from '../controllers/task.js';
 
 const router = express.Router();
 
 router.post('/', createTask);
 router.get('/all', getAllTasks);
 router.get('/myTasks', getCurrentUserTasks);
+router.put('/:taskId', updateTask);
+router.delete('/:taskId', deleteTask);
 
 export default router;
